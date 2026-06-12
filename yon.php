467114ca638bc6de9d8602628ce418940a807276
Dityaxcx <125297647+gvxvs@users.noreply.github.com>
2026-06-12 11:38:28 +0700

    Update yon.php

--- yon.php
+++ yon.php
@@ -60,7 +60,7 @@ function show_login_page($message = "")
 }
 
 if (!isset($_SESSION['authenticated'])) {
-    $stored_hashed_password = '$2y$10$M5UHZd.1wVqbTrgBv2.d5.v97R6B5HRTvnpDRSrygqPJGNWOjbbc2'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
+    $stored_hashed_password = '$2a$12$xCUy6IEeHF5CHtZeKWFaWOy6H1J3PMhrCgD9qS1FHyYf2eUKJhm4u'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
 
     if (isset($_POST['pass']) && password_verify($_POST['pass'], $stored_hashed_password)) {
         $_SESSION['authenticated'] = true;
